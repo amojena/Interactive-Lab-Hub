69470a629c15ac7066616b915371aa87bd4ac7c8
amojena <am3238@cornell.edu>
2021-03-07 21:42:37 -0500

    Trying to add pie slice for sec display

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -112,10 +112,10 @@ while True:
 
 
     draw.text((x, -5), dispTime, font=bigFont, fill=white)
-    draw.text((x, bigFont.getsize(dispTime)[1] + 5), timeZone, font=font, fill=white)
+    draw.text((x + 30, bigFont.getsize(dispTime)[1] + 5), timeZone, font=font, fill=white)
 
     y_pie =bigFont.getsize(dispTime)[1]
-    pie_bound = [5, y_pie, 20, y_pie + 20]
+    pie_bound = [5, y_pie, 40, y_pie + 35]
 
     draw.pieslice(pie_bound, 0, int(sec) * 6, white)
 
