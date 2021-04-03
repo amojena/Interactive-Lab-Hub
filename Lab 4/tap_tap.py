36ec13a5e6d30d4ac93726f7bff2a7c3e75db680
amojena <am3238@cornell.edu>
2021-04-03 19:59:33 -0400

    figuring out starting pos for circles

--- Lab 4/tap_tap.py
+++ Lab 4/tap_tap.py
@@ -4,7 +4,6 @@ import digitalio
 import board
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_rgb_display.st7789 as st7789
-from image_fn import main
 
 
 # Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
