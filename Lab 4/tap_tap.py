70166728628e3cecf43b7ff5e6e95b90f6403482
amojena <am3238@cornell.edu>
2021-04-03 20:03:01 -0400

    figuring out starting pos for circles

--- Lab 4/tap_tap.py
+++ Lab 4/tap_tap.py
@@ -76,10 +76,10 @@ def main():
     while True:
         # Draw a black filled box to clear the image.
         draw.rectangle((0, 0, width, height), outline=0, fill=0)
-
-        input_x = int(input("X value:"))
-        offset = 10
-        draw.ellipse([(input_x, 5), (input_x+offset,15)], fill="#FF0000", outline="#0000FF")
+        # input_x = int(input("X value:"))
+        x = 220
+        offset = input("X offset: ")
+        draw.ellipse([(x, 5), (x+offset,15)], fill="#FF0000", outline="#0000FF")
 
         # Display image.
         disp.image(image, rotation)
