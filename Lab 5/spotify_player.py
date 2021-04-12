2bbe1d97dcfefbce8e92d2f74bd36b25dcaf1c00
amojena <am3238@cornell.edu>
2021-04-12 17:39:46 -0400

    Trying to load model

--- Lab 5/spotify_player.py
+++ Lab 5/spotify_player.py
@@ -1,21 +1,10 @@
-import Time
+import time
 import os
 import system
 import numpy as np
 import subprocess
-
-CONFIDENCE_THRESHOLD = 0.5
-PERSISTANCE_THRESHOLD = 0.5
-
-
-from rpi_vision.agent.capture import PiCameraStream
-from rpi_vision.models.teachablemachine import TeachableMachine
-
-capture_manager = PiCameraStream(resolution=(screen.get_width(), screen.get_height()), rotation=180, preview=False)
+import tensorflow.keras as tf
 
 if __name__ == '__main__':
-    model = TeachableMachine('transfer/converted_savedmodel.zip')
-    capture_manager.start()
-
-    while not capture.manager_stopped:
-        print("Cam working")
+    savedModelPath = "/transfer/converted_savedmodel.zip"
+    tf.keras.models.load_model(savedModelPath)
