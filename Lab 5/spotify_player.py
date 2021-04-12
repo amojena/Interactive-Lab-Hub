b78e9279d572b54dfa80f79252cf1e3a4b1b3f2e
amojena <am3238@cornell.edu>
2021-04-12 17:50:24 -0400

    new model

--- Lab 5/spotify_player.py
+++ Lab 5/spotify_player.py
@@ -6,5 +6,5 @@ import subprocess
 import tensorflow.keras as tf
 
 if __name__ == '__main__':
-    savedModelPath = "/transfer/converted_savedmodel.zip"
+    savedModelPath = "/transfer/converted_savedmodel/model.savemodel/saved_model.pb"
     tf.models.load_model(savedModelPath)
