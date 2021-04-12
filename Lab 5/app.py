7ca3227bde79d52d7f4cc4c37a1710ce1f140b4b
amojena <am3238@cornell.edu>
2021-04-11 22:42:57 -0400

    Avg and peaks for accelerometer

--- Lab 5/app.py
+++ Lab 5/app.py
@@ -31,12 +31,12 @@ if __name__ == "__main__":
         yTotal += mpu.acceleration[1]
         zTotal += mpu.acceleration[2]
 
-        print(f"Average after {round}s: ({xTotal/round},{yTotal/round},{zTotal/round})", flush=True)
+        print(f"Average after {round}s: ({xTotal/round},{yTotal/round},{zTotal/round})\r", end="" flush=True)
         round += 1
 
         xPeak = updatePeak(xPeak, mpu.acceleration[0])
         yPeak = updatePeak(yPeak, mpu.acceleration[1])
         zPeak = updatePeak(zPeak, mpu.acceleration[2])
 
-        print(f"Peaks X: {xPeak}, Y: {yPeak}, Z: {zPeak}", end="\r", flush=True)
+        print(f"Peaks X: {xPeak}, Y: {yPeak}, Z: {zPeak}")
         time.sleep(1)
