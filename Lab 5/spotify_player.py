4891309c2e812e1a76ae6ea2de5931dc9176b712
ilan <ilan.mandel1@gmail.com>
2021-04-12 22:52:55 -0400

    Trying to get spotipy to work

--- Lab 5/spotify_player.py
+++ Lab 5/spotify_player.py
@@ -1,66 +1,24 @@
-import time
-import os
-import sys
-import numpy as np
-import subprocess
-import tensorflow.keras as tf
-from picamera import PiCamera
-from PIL import Image, ImageOps
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 
-labels = {0: "Previous", 1: "Next", 2: "Pause/Play", 3: "Neutral"}
-
-def interpret(prediction):
-    p = -1
-    pi = -1
-    for i, val in enumerate(prediction[0]):
-        if val > p:
-            p = val
-            pi = i
+def getCreds():
+    filename = input("creds filename: ")
+    filename = filename + ".txt"
     
-    print(f"{labels[pi]}: {p*100}%")
-
-if __name__ == '__main__':
-    print("Loading...")
+    with open(filename) as f:
+        lines = f.readlines()
     
-    savedModelPath = "transfer/converted_keras/keras_model.h5"
-    model = tf.models.load_model(savedModelPath)
+    return lines[0].rstrip(), lines[1].rstrip(), lines[2].rstrip()
 
-    
-    img_file = "test2.jpg"
-    cmd = "fswebcam " + img_file
-    for i in range(5):
-        time.sleep(3)
-        os.system(cmd)
-        
-        # Create the array of the right shape to feed into the keras model
-        # The 'length' or number of images you can put into the array is
-        # determined by the first position in the shape tuple, in this case 1.
-        data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+def without(clientID, secret):
+    return spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=clientID, client_secret=secret))
 
-        # Replace this with the path to your image
-        image = Image.open(img_file)
+def withAuth(clientID, secret, redi):
+    return spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=clientID, client_secret=secret, redirect_uri=redi))
 
-        #resize the image to a 224x224 with the same strategy as in TM2:
-        #resizing the image to be at least 224x224 and then cropping from the center
-        size = (224, 224)
-        image = ImageOps.fit(image, size, Image.ANTIALIAS)
-
-        #turn the image into a numpy array
-        image_array = np.asarray(image)
-
-        # display the resized image
-        #image.show()
-
-        # Normalize the image
-        normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-
-        # Load the image into the array
-        data[0] = normalized_image_array
-
-        # run the inference
-        prediction = model.predict(data)
-        print(prediction)
-        interpret(prediction)
-        print()
-        print()
-    
+if __name__ == "__main__":
+    clientID, secret, redi = getCreds()
+    sp = withAuth(clientID, secret,redi)
+    print(sp.current_user_playing_track())
+     
