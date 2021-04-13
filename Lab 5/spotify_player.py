bc6b1a602ac244b0e0d80f8c496179e3b41415c1
amojena <am3238@cornell.edu>
2021-04-12 20:09:20 -0400

    Model import maybe works? trying cam picture

--- Lab 5/spotify_player.py
+++ Lab 5/spotify_player.py
@@ -4,9 +4,14 @@ import sys
 import numpy as np
 import subprocess
 import tensorflow.keras as tf
+from picamera import picCamera
 
 if __name__ == '__main__':
     print("Loading...")
     savedModelPath = "transfer/converted_keras/keras_model.h5"
     model = tf.models.load_model(savedModelPath)
+
+    cam = PiCamera()
+
+    cam.capture('test.jpg')
     print("Yay")
