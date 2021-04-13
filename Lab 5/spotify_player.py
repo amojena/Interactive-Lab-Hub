4c72812610fad7ed30b9265a3d7da74163829f9e
amojena <am3238@cornell.edu>
2021-04-12 20:15:05 -0400

    Trying prediction

--- Lab 5/spotify_player.py
+++ Lab 5/spotify_player.py
@@ -5,6 +5,7 @@ import numpy as np
 import subprocess
 import tensorflow.keras as tf
 from picamera import PiCamera
+from PIL import Image, ImageOps
 
 if __name__ == '__main__':
     print("Loading...")
@@ -13,5 +14,37 @@ if __name__ == '__main__':
 
     cam = PiCamera()
 
-    cam.capture('test.jpg')
-    print("Yay")
+    # cam.capture('test.jpg')
+
+
+
+    # Create the array of the right shape to feed into the keras model
+    # The 'length' or number of images you can put into the array is
+    # determined by the first position in the shape tuple, in this case 1.
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+    # Replace this with the path to your image
+    image = Image.open('test.jpg')
+
+    #resize the image to a 224x224 with the same strategy as in TM2:
+    #resizing the image to be at least 224x224 and then cropping from the center
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+    #turn the image into a numpy array
+    image_array = np.asarray(image)
+
+    # display the resized image
+    # image.show()
+
+    # Normalize the image
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+
+    # Load the image into the array
+    data[0] = normalized_image_array
+
+    # run the inference
+    prediction = model.predict(data)
+    print(prediction)
+
+    
