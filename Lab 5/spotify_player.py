e65a645428d5fa4fe869b344ddbef983e6d29a55
ilan <ilan.mandel1@gmail.com>
2021-04-12 21:38:12 -0400

    Have to retrain the model

--- Lab 5/spotify_player.py
+++ Lab 5/spotify_player.py
@@ -7,44 +7,60 @@ import tensorflow.keras as tf
 from picamera import PiCamera
 from PIL import Image, ImageOps
 
+labels = {0: "Previous", 1: "Next", 2: "Stop", 3: "Play", 4: "Neutral"}
+
+def interpret(prediction):
+    p = -1
+    pi = -1
+    for i, val in enumerate(prediction[0]):
+        if val > p:
+            p = val
+            pi = i
+    
+    print(f"{labels[pi]}: {p*100}%")
+
 if __name__ == '__main__':
     print("Loading...")
+    
     savedModelPath = "transfer/converted_keras/keras_model.h5"
     model = tf.models.load_model(savedModelPath)
 
-    cam = PiCamera()
+    
+    img_file = "test2.jpg"
+    cmd = "fswebcam " + img_file
+    for i in range(5):
+        time.sleep(3)
+        os.system(cmd)
+        
+        # Create the array of the right shape to feed into the keras model
+        # The 'length' or number of images you can put into the array is
+        # determined by the first position in the shape tuple, in this case 1.
+        data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-    # cam.capture('test.jpg')
+        # Replace this with the path to your image
+        image = Image.open(img_file)
 
+        #resize the image to a 224x224 with the same strategy as in TM2:
+        #resizing the image to be at least 224x224 and then cropping from the center
+        size = (224, 224)
+        image = ImageOps.fit(image, size, Image.ANTIALIAS)
 
+        #turn the image into a numpy array
+        image_array = np.asarray(image)
 
-    # Create the array of the right shape to feed into the keras model
-    # The 'length' or number of images you can put into the array is
-    # determined by the first position in the shape tuple, in this case 1.
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+        # display the resized image
+        #image.show()
 
-    # Replace this with the path to your image
-    image = Image.open('test.jpg')
+        # Normalize the image
+        normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
 
-    #resize the image to a 224x224 with the same strategy as in TM2:
-    #resizing the image to be at least 224x224 and then cropping from the center
-    size = (224, 224)
-    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+        # Load the image into the array
+        data[0] = normalized_image_array
 
-    #turn the image into a numpy array
-    image_array = np.asarray(image)
-
-    # display the resized image
-    # image.show()
-
-    # Normalize the image
-    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-
-    # Load the image into the array
-    data[0] = normalized_image_array
-
-    # run the inference
-    prediction = model.predict(data)
-    print(prediction)
-
+        # run the inference
+        prediction = model.predict(data)
+        print(prediction)
+        interpret(prediction)
+        print()
+        print()
     
