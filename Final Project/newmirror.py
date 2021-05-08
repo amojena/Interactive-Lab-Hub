ed81201cc64aab7037fe7f8a96bc39b523b2ad5f
anam884 <at884@cornell.edu>
2021-05-08 14:17:13 -0400

    newmirror

---
+++ Final Project/newmirror.py
@@ -0,0 +1,99 @@
+# smartmirror.py
+# requirements
+# requests, feedparser, traceback, Pillow
+
+from tkinter import *
+import locale
+import threading
+import time
+import requests
+import json
+import traceback
+import feedparser
+
+from PIL import Image, ImageTk
+from contextlib import contextmanager
+
+LOCALE_LOCK = threading.Lock()
+
+ui_locale = '' # e.g. 'fr_FR' fro French, '' as default
+time_format = 12 # 12 or 24
+date_format = "%b %d, %Y" # check python doc for strftime() for options
+news_country_code = 'us'
+weather_api_token = '<TOKEN>' # create account at https://darksky.net/dev/
+weather_lang = 'en' # see https://darksky.net/dev/docs/forecast for full list of language parameters values
+weather_unit = 'us' # see https://darksky.net/dev/docs/forecast for full list of unit parameters values
+latitude = None # Set this if IP location lookup does not work for you (must be a string)
+longitude = None # Set this if IP location lookup does not work for you (must be a string)
+xlarge_text_size = 94
+large_text_size = 48
+medium_text_size = 28
+small_text_size = 18
+
+@contextmanager
+def setlocale(name): #thread proof function to work with locale
+    with LOCALE_LOCK:
+        saved = locale.setlocale(locale.LC_ALL)
+        try:
+            yield locale.setlocale(locale.LC_ALL, name)
+        finally:
+            locale.setlocale(locale.LC_ALL, saved)
+
+# maps open weather icons to
+# icon reading is not impacted by the 'lang' parameter
+icon_lookup = {
+    'clear-day': "assets/Sun.png",  # clear sky day
+    'wind': "assets/Wind.png",   #wind
+    'cloudy': "assets/Cloud.png",  # cloudy day
+    'partly-cloudy-day': "assets/PartlySunny.png",  # partly cloudy day
+    'rain': "assets/Rain.png",  # rain day
+    'snow': "assets/Snow.png",  # snow day
+    'snow-thin': "assets/Snow.png",  # sleet day
+    'fog': "assets/Haze.png",  # fog day
+    'clear-night': "assets/Moon.png",  # clear sky night
+    'partly-cloudy-night': "assets/PartlyMoon.png",  # scattered clouds night
+    'thunderstorm': "assets/Storm.png",  # thunderstorm
+    'tornado': "assests/Tornado.png",    # tornado
+    'hail': "assests/Hail.png"  # hail
+    # 'shirt': "assests/unnamed.png"
+}
+
+
+
+
+class FullscreenWindow:
+
+    def __init__(self):
+        self.tk = Tk()
+        self.tk.configure(background='black')
+        self.topFrame = Frame(self.tk, background = 'black')
+        self.bottomFrame = Frame(self.tk, background = 'black')
+        self.topFrame.pack(side = TOP, fill=BOTH, expand = YES)
+        self.bottomFrame.pack(side = BOTTOM, fill=BOTH, expand = YES)
+        self.state = False
+        self.tk.bind("<Return>", self.toggle_fullscreen)
+        self.tk.bind("<Escape>", self.end_fullscreen)
+        image1 = Image.open("assets/unnamed.png")
+        test = ImageTk.PhotoImage(image1)
+
+        label1 = Label(bg='black', image=test)
+        label1.image = test
+
+        # Position image
+        label1.place(x=100, y=0)
+
+
+    def toggle_fullscreen(self, event=None):
+        self.state = not self.state  # Just toggling the boolean
+        self.tk.attributes("-fullscreen", self.state)
+        return "break"
+
+    def end_fullscreen(self, event=None):
+        self.state = False
+        self.tk.attributes("-fullscreen", False)
+        return "break"
+
+if __name__ == '__main__':
+    w = FullscreenWindow()
+    # root.mainloop()
+    w.tk.mainloop()
