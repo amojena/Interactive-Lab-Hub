bdae3c7ba172330da395d4ca726757b8d7a5d851
anam884 <at884@cornell.edu>
2021-05-08 16:12:48 -0400

    camera

--- Final Project/camera.py
+++ Final Project/camera.py
@@ -3,38 +3,78 @@
 import tensorflow.keras
 from PIL import Image, ImageOps
 import numpy as np
+import cv2
+import sys
+
 
 # Disable scientific notation for clarity
 np.set_printoptions(suppress=True)
 
+img = None
+webCam = False
+if(len(sys.argv)>1 and not sys.argv[-1]== "noWindow"):
+   try:
+      print("I'll try to read your image");
+      img = cv2.imread(sys.argv[1])
+      if img is None:
+         print("Failed to load image file:", sys.argv[1])
+   except:
+      print("Failed to load the image are you sure that:", sys.argv[1],"is a path to an image?")
+else:
+   try:
+      print("Trying to open the Webcam.")
+      cap = cv2.VideoCapture(0)
+      if cap is None or not cap.isOpened():
+         raise("No camera")
+      webCam = True
+   except:
+      img = cv2.imread("../data/test.jpg")
+      print("Using default image.")
+
+
 # Load the model
 model = tensorflow.keras.models.load_model('keras_model.h5')
+# Load Labels:
+labels=[]
+f = open("labels.txt", "r")
+for line in f.readlines():
+    if(len(line)<1):
+        continue
+    labels.append(line.split(' ')[1].strip())
 
-# Create the array of the right shape to feed into the keras model
-# The 'length' or number of images you can put into the array is
-# determined by the first position in the shape tuple, in this case 1.
-data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-# Replace this with the path to your image
-image = Image.open('/home/pi/openCV-examples/data/test.jpg')
+while(True):
+    if webCam:
+        ret, img = cap.read()
 
-#resize the image to a 224x224 with the same strategy as in TM2:
-#resizing the image to be at least 224x224 and then cropping from the center
-size = (224, 224)
-image = ImageOps.fit(image, size, Image.ANTIALIAS)
+    rows, cols, channels = img.shape
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-#turn the image into a numpy array
-image_array = np.asarray(image)
+    image = Image.open('/home/pi/openCV-examples/data/test.jpg')
+    size = (224, 224)
+    img =  cv2.resize(img, size, interpolation = cv2.INTER_AREA)
+    #turn the image into a numpy array
+    image_array = np.asarray(img)
 
-# display the resized image
-image.show()
+    # Normalize the image
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    # Load the image into the array
+    data[0] = normalized_image_array
 
-# Normalize the image
-normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    # run the inference
+    prediction = model.predict(data)
+    print("I think its a:",labels[np.argmax(prediction)])
 
-# Load the image into the array
-data[0] = normalized_image_array
+    if webCam:
+        if sys.argv[-1] == "noWindow":
+           cv2.imwrite('detected_out.jpg',img)
+           continue
+        cv2.imshow('detected (press q to quit)',img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cap.release()
+            break
+    else:
+        break
 
-# run the inference
-prediction = model.predict(data)
-print(prediction)
+cv2.imwrite('detected_out.jpg',img)
+cv2.destroyAllWindows()
