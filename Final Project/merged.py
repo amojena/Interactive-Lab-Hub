3758fc3c91e19aaa3d71dfc3ad625f301dae7629
anam884 <at884@cornell.edu>
2021-05-08 18:39:48 -0400

    merge

--- Final Project/merged.py
+++ Final Project/merged.py
@@ -1,15 +1,21 @@
 from tkinter import *
 import locale
 import threading
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageOps
 from contextlib import contextmanager
 from random import choice
+import tensorflow.keras
+import numpy as np
+import cv2
+import sys
+import time
 
 from os import listdir
 
 import time
 import board
 import busio
+
 
 import adafruit_mpr121
 
@@ -26,6 +32,15 @@ xlarge_text_size = 94
 large_text_size = 48
 medium_text_size = 28
 small_text_size = 18
+impressio_times=[]
+class flag_manager:
+    def __init__ (self):
+        self.is_face=False
+        self.face_counter = 0
+        self.t_start=0
+        self.t_start_flag=False
+        self.t_end=0
+fm=flag_manager()
 
 @contextmanager
 def setlocale(name): #thread proof function to work with locale
@@ -39,9 +54,87 @@ def setlocale(name): #thread proof function to work with locale
 # maps open weather icons to
 # icon reading is not impacted by the 'lang' parameter
 
+# *********** Load Camera Module ***************
+# Disable scientific notation for clarity
+np.set_printoptions(suppress=True)
+def Average(lst):
+  if len(lst) != 0:
+    return sum(lst) / len(lst)
+
+img = None
+webCam = False
+if(len(sys.argv)>1 and not sys.argv[-1]== "noWindow"):
+   try:
+      print("I'll try to read your image");
+      img = cv2.imread(sys.argv[1])
+      if img is None:
+         print("Failed to load image file:", sys.argv[1])
+   except:
+      print("Failed to load the image are you sure that:", sys.argv[1],"is a path to an image?")
+else:
+   try:
+      print("Trying to open the Webcam.")
+      cap = cv2.VideoCapture(0)
+      if cap is None or not cap.isOpened():
+         raise("No camera")
+      webCam = True
+   except:
+      img = cv2.imread("../data/test.jpg")
+      print("Using default image.")
 
 
+# Load the model
+model = tensorflow.keras.models.load_model('keras_model.h5')
+# Load Labels:
+labels=[]
+f = open("labels.txt", "r")
+for line in f.readlines():
+    if(len(line)<1):
+        continue
+    labels.append(line.split(' ')[1].strip())
 
+# *****************
+
+def run_camera():
+    if webCam:
+        ret, img = cap.read()
+
+    rows, cols, channels = img.shape
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+    image = Image.open('/home/pi/openCV-examples/data/test.jpg')
+    size = (224, 224)
+    img =  cv2.resize(img, size, interpolation = cv2.INTER_AREA)
+    #turn the image into a numpy array
+    image_array = np.asarray(img)
+
+    # Normalize the image
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    # Load the image into the array
+    data[0] = normalized_image_array
+    # run the inference
+    prediction = model.predict(data)
+    detected = labels[np.argmax(prediction)]
+    
+    if detected == "face":
+      if fm.is_face == False:
+        print("***************************new face detected")
+        fm.face_counter+=1
+        fm.is_face = True
+        fm.t_start_flag=True
+        fm.t_start = time.time()
+
+    else:
+      fm.is_face = False
+      if fm.t_start_flag:
+        print("person gone, adding time")
+        fm.t_end=time.time()
+        impressio_times.append(fm.t_end-fm.t_start)
+        fm.t_start_flag=False
+
+      
+    print("face counter ="+str(fm.face_counter))
+    print (Average(impressio_times))
 
 
 class FullscreenWindow:
@@ -106,6 +199,7 @@ if __name__ == '__main__':
     w = FullscreenWindow()
     # w.tk.mainloop()
     while True:
+        run_camera()
         w.tk.update_idletasks()
         w.tk.update()
         w.check_touch()
